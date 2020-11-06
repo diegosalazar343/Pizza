bfc3c7d4c63ee9e73cbe7ce66aa2784a9b3863e8
diegosalazar343 <diegosalazar343@gmail.com>
2020-11-06 11:45:20 -0800

    add form for pizza size, add values to forms

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,17 @@ function Account(name, order, total) {
   this.total = 0;
 }
 
+Account.prototype.pizzaPush = function(newAccount) {
+  this.total = this.order;
+}
+
+function determineAmount(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15, input16, input17, input18)
+
+const valTotal = input1+ input2 + input3 + input4 + input5
++ input6 + input7 + input8 + input9 + input10 + input11 + input12 + input13 + input14 + input15 + input16 + input17 + input18;
+Account.prototype.pizzaOrder = function(prettyPizza) {
+  if (valTotal <= )
+}
 
 //Users Logic
 $(document).ready(function(){
@@ -13,6 +24,8 @@ $(document).ready(function(){
 
     const name= $("input#name").val();
     $("#name-id").show()
+    let newAccount = new Account(name);
+
 
   })
 })
