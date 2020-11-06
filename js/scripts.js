d0cd8aad71b9f7255349a52b45b1c4afb1c64b36
diegosalazar343 <diegosalazar343@gmail.com>
2020-11-06 13:35:12 -0800

    move my determine amount function into pizzaorder prototype

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,17 @@ Account.prototype.pizzaPush = function(newAccount) {
   this.total = this.order;
 }
 
-function determineAmount(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15, input16, input17, input18)
 
-const valTotal = input1+ input2 + input3 + input4 + input5
-+ input6 + input7 + input8 + input9 + input10 + input11 + input12 + input13 + input14 + input15 + input16 + input17 + input18;
-Account.prototype.pizzaOrder = function(prettyPizza) {
+
+Account.prototype.pizzaOrder = function determineAmount(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15, input16, input17, input18) {
+  const valTotal = input1 + input2 + input3 + input4 + input5
+    + input6 + input7 + input8 + input9 + input10 + input11 + input12 + input13 + input14 + input15 + input16 + input17 + input18;
   if (valTotal <= )
 }
 
 //Users Logic
 $(document).ready(function(){
-  $("form#account").submit(functin(event) {
+  $("form#account").submit(function(event) {
     event.preventDefault();
 
     const name= $("input#name").val();
