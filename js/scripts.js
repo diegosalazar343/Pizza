196b0ea3fa6d393502e9cfae956da05faf6dc255
diegosalazar343 <diegosalazar343@gmail.com>
2020-11-06 13:44:10 -0800

    begin user interface for pizza order form

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,19 @@ function Account(name, order, total) {
   this.total = 0;
 }
 
-Account.prototype.pizzaPush = function(newAccount) {
-  this.total = this.order;
-}
-
-
-
 Account.prototype.pizzaOrder = function determineAmount(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15, input16, input17, input18) {
   const valTotal = input1 + input2 + input3 + input4 + input5
     + input6 + input7 + input8 + input9 + input10 + input11 + input12 + input13 + input14 + input15 + input16 + input17 + input18;
-  if (valTotal <= )
+  if (valTotal <= 6){
+    this.total = "$7";
+  } else if (valTotal <= 8){
+    this.total = "$10";
+  } else if (valTotal <= 13){
+    this.total = "$15";
+  } else {
+    this.total = "$20";
+  }
+  return this.total;
 }
 
 //Users Logic
@@ -25,7 +28,10 @@ $(document).ready(function(){
     const name= $("input#name").val();
     $("#name-id").show()
     let newAccount = new Account(name);
+  })
+  $("form#pizza").submit(function(event){
+    event.preventDefault();
 
-
+    let
   })
 })
