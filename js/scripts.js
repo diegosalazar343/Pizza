b058e3ace35ae5c4bece0b321daf94dce86d3613
diegosalazar343 <diegosalazar343@gmail.com>
2020-11-06 10:33:28 -0800

    create construct for pizza account

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 //Business Logic
-
+function Account(name, order, total) {
+  this.name = name;
+  this.order = [];
+  this.total = 0;
+}
 
 
 //Users Logic
 $(document).ready(function(){
-  $("form#account").submit(functin(event){
+  $("form#account").submit(functin(event) {
     event.preventDefault();
-    $("#name-id")
+
+    const name= $("input#name").val();
+    $("#name-id").show()
+
   })
 })
